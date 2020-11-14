7b0cb5ea7f4620a48827a98d4f72a59ec591ae00
jirapornNgewdang <jiraporn7494@gmail.com>
2020-11-14 19:52:29 +0700

    register numberone

--- indexRegister.php
+++ indexRegister.php
@@ -1,40 +1,32 @@
-<?php 
+<?php
+
     include_once('function.php'); 
     $userdata  = new DB_con();
     if(isset($_POST['submit'])){
         $fname = $_POST['fullname'];
         $uname = $_POST['username'];
         $uemail = $_POST['email'];
-        $password = $_POST['password'];
+        $password = md5($_POST['password']);
         
-        $sql = $userdata->registration($fname,$uname,$uemail,$password);
+        $sql = $userdata->registration($fname, $uname, $uemail, $password);
 
         if ($sql) {
-            echo "<script>alert('Registor Successfull');</script>";
-            echo "<script>window.location.href='signin.php'</script>";
-        }  else {
-            echo "<script>alert('Something went wrong! Please try again');</script>";
-            echo "<script>window.location.href='signin.php'</script>";
-        }
+          echo "<script>alert('Registor Successful!');</script>";
+          echo "<script>window.location.href='signin.php'</script>";
+      } else {
+          echo "<script>alert('Something went wrong! Please try again.');</script>";
+          echo "<script>window.location.href='signin.php'</script>";
+      }
     }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>OTOP Register Page</title>
-        <script>
-            function checkusername(val){
-              $.ajax({
-                  type:'POST'
-                  url:'checkuser_availble.php'
-                  data: 'username='+val,
-                  success: function(data){
-                     $('#usernameavailble').html(data);
-                  }
-              });
-            }
-        </script>
+        
         <link rel="stylesheet" href="stylesheet.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -45,18 +37,30 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto:wght@300&display=swap" rel="stylesheet">
     </head>
     <body>
+    <script>
+            function checkusername(val){
+              $.ajax({
+                  type:'POST'
+                  url:'checkuser_available.php'
+                  data: 'username='+val,
+                  success: function(data){
+                     $('#usernameavailable').html(data);
+                  }
+              });
+            }
+        </script>
      <div class="container">
          <h1 class="mt-5">Register Otop shopping online</h1>
          <hr>
      <form method="post">
   <div class="form-group">
     <label for="fullname" class="form-label">Full Name</label>
-    <input type="text" class="form-control" id="fullname" placeholder="fullname" name="fullname">
+    <input type="text" class="form-control" id="username" placeholder="fullname" name="fullname">
   </div>
   <div class="form-group">
     <label for="username" class="form-label">User Name</label>
     <input type="text" class="form-control" id="username" placeholder="username" name="username" onblur="checkusername(this.value)" >
-    <span id="usernameavailble"></span>
+    <span id="usernameavailable"></span>
   </div>
   <div class="form-group">
     <label for="email" class="form-label">Email</label>
---
+++ register.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 13, 2020 at 03:38 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `register`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `useremail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `regdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tblusers`
+--
+
+INSERT INTO `tblusers` (`id`, `fullname`, `username`, `useremail`, `password`, `regdate`) VALUES
+(1, 'milk', 'milkza', 'jiraporn1301@gmail.com', '1234', '2020-11-13 14:00:37');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblusers`
+--
+ALTER TABLE `tblusers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
