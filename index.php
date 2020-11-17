29c6c9960fda1122e3b34fe00b20f2150b2a7fa7
rintori <spatanlol.25@gmail.com>
2020-11-17 16:41:00 +0700

    add

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
 </head>
 
 <body>
+<p>mew</p>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#"><img src="image/icon.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
